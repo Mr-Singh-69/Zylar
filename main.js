267281de871ff8af055cc31fd54e9848aec42abe
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-01-02 15:02:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const keepAlive = require('./keep_alive');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent] });
 client.commands = new Collection();
 
-// Actual code
-// Do It Yourself
+
 
 keepAlive();
 
