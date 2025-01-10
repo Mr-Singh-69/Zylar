950403c72a1f1d5a205cc45a279b457e43e4bf73
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-01-10 20:00:16 +0530

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'messageCreate',
+    execute(message, bot) {
+        if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = bot.commands.get(commandName);
+        if (!command) return;
+
+        try {
+            command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('There was an error executing that command.');
+        }
+    },
+};
