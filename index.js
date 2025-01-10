34d0c6bc3c1ca6a8d5c2cd607f4ced891e14a698
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-01-10 20:02:44 +0530

    Update and rename main.js to index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const config = require('./config.json');
+
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+bot.commands = new Collection();
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+}
+
+const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        bot.once(event.name, (...args) => event.execute(...args, bot));
+    } else {
+        bot.on(event.name, (...args) => event.execute(...args, bot));
+    }
+}
+
+bot.login(config.token);
+
+// Express server
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Webpage running at http://localhost:${PORT}`);
+});
